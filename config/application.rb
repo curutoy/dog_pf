6c7025677339ca0a002e9c9cd935bfc88ecc146e
Mariko Katsuno <curureeno@gmail.com>
2020-11-10 15:26:12 +0900

    Change: apprication.rb_config.g

--- config/application.rb
+++ config/application.rb
@@ -15,12 +15,12 @@ module DogPf
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
 
-  config.generators do |g|
-    g.test_framework :rspec, 
-      view_specs: false, 
-      controller_specs: false, 
-      routing_specs: false
+    config.generators do |g|
+      g.test_framework :rspec, 
+        view_specs: false, 
+        controller_specs: false, 
+        routing_specs: false
+    end
   end
 end
