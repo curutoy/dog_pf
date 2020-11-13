74f4e63a55dd21d111f8a4bc04ed1a02c8c32923
Mariko Katsuno <curureeno@gmail.com>
2020-11-13 17:43:08 +0900

    Set: omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+      user.image = auth.info.image
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   root 'dogs#index'
 
   devise_for :protectors, controllers: {
-    sessions:      'protectors/sessions',
-    passwords:     'protectors/passwords',
-    registrations: 'protectors/registrations'
+    sessions: 'protectors/sessions',
+    passwords: 'protectors/passwords',
+    registrations: 'protectors/registrations',
   }
+
   devise_for :users, controllers: {
-    sessions:           'users/sessions',
-    passwords:          'users/passwords',
-    registrations:      'users/registrations',
-    omniauth_callbacks: 'users/omniauth_callbacks'
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks',
   }
 end
