7a51d85f562c37f29b594cb22ec6d2a4179b43d1
Mariko Katsuno <curureeno@gmail.com>
2020-11-13 22:26:16 +0900

    Add: validation

--- app/models/protector.rb
+++ app/models/protector.rb
@@ -1,6 +1,11 @@
 class Protector < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name,    presence: true,
+                      length: { maximum: 20 }
+  validates :email,   presence: true,
+                      length: { maximum: 60 }
+  validates :address, presence: true
+  validates :profile, length: { maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+
+  validates :name,          presence: true,
+                            length: { maximum: 20 }
+  validates :email,         presence: true,
+                            length: { maximum: 60 }
+  validates :address,       presence: true
+  validates :family_people, presence: true
+  validates :house,         presence: true
+  validates :caretaker,     presence: true
+  validates :profile,       length: { maximum: 200 }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
