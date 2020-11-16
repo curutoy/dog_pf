000199a73d485fbc46e5ee7197b109d38fa99cb6
Mariko Katsuno <curureeno@gmail.com>
2020-11-16 11:26:03 +0900

    set_omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.name = auth.info.name
       user.email = auth.info.email
-      user.image = auth.info.image
       user.password = Devise.friendly_token[0, 20]
     end
   end
