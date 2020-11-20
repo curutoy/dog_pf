bd7ad0a251b577a10c19e5ac50f33f065e7aa407
Mariko Katsuno <curureeno@gmail.com>
2020-11-20 11:13:36 +0900

    Change: devise_jp

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module DogPf
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.i18n.default_locale = :ja
+    
     config.generators do |g|
       g.test_framework :rspec, 
         view_specs: false, 
