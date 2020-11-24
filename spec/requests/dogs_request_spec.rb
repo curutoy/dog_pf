43a89b933bcb9c58d95d91cbbb9ffd16fbd13f9b
Mariko Katsuno <curureeno@gmail.com>
2020-11-25 00:37:52 +0900

    Rspec: root_path-requests.spec

--- spec/requests/dogs_request_spec.rb
+++ spec/requests/dogs_request_spec.rb
@@ -1,10 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "Dogs", type: :request do
+  let!(:user) { create(:user) }
+  let!(:protector) { create(:protector) }
+
   describe "GET /index" do
-    it "returns http success" do
-      get "/dogs/index"
-      expect(response).to have_http_status(:success)
+    context "ログインしていない場合" do
+      before do
+        get root_path
+      end
+
+      it "リクエストが成功すること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "home/indexテンプレートが表示されること" do
+        expect(response).to render_template "home/index"
+      end
+    end
+
+    context "userでログイン状態の場合" do
+      before do
+        sign_in user
+        get root_path
+      end
+
+      it "リクエストが成功すること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "showテンプレートが表示されること" do
+        expect(response).to render_template :index
+      end
+    end
+
+    context "protectorでログイン状態の場合" do
+      before do
+        sign_in protector
+        get root_path
+      end
+
+      it "リクエストが成功すること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "showテンプレートが表示されること" do
+        expect(response).to render_template :index
+      end
     end
   end
 end
