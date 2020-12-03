40945206e4c5351d5bb86c62d0114ad019939886
Mariko Katsuno <curureeno@gmail.com>
2020-12-03 21:22:03 +0900

    Rspec: post_model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :post do
-    content { "MyText" }
-    dog { nil }
+    content { "post content" }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:protector) { create(:protector) }
+  let!(:dog) { build(:dog, protector_id: protector.id) }
+  let!(:testpost) { build(:post) }
+  let(:testpost2) { build(:post) }
+
+  describe "validation" do
+    before do
+      dog.image = fixture_file_upload("/files/test.png")
+      dog.save
+      testpost.dog_id = dog.id
+      testpost.image = fixture_file_upload("/files/test.png")
+    end
+
+    it "全て入力済みであればバリデーションエラーは発生しないこと" do
+      expect(testpost).to be_valid
+    end
+
+    it "imageを選択していない場合エラーが発生すること" do
+      testpost2.dog_id = dog.id
+      expect(testpost2).to be_invalid
+    end
+
+    it "contentが空であってもエラーは発生しないこと" do
+      testpost.content = ""
+      expect(testpost).to be_valid
+    end
+
+    it "contentが200文字以上の場合エラーが発生すること" do
+      testpost.content = "a " * 201
+      expect(testpost).to be_invalid
+    end
+  end
 end
