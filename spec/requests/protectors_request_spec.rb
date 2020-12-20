8ac37d572e6bdf1680e5155bc8edf0272907e05c
Mariko Katsuno <curureeno@gmail.com>
2020-12-20 23:38:10 +0900

    Spec: user/protector_message

--- spec/requests/protectors_request_spec.rb
+++ spec/requests/protectors_request_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Protectors", type: :request do
   let!(:protector)    { create(:protector) }
+  let!(:user)         { create(:user) }
   let!(:dogs)         { create_list(:dog2, 2, protector_id: protector.id) }
-  let!(:relationship) { create(:relationship, protector: protector) }
+  let!(:relationship) { create(:relationship, protector: protector, user: user) }
+  let!(:room)         { create(:room) }
+  let!(:entry)        { create(:entry, user: user, protector: protector, room: room) }
 
   describe "GET /show" do
     context "ログインしていない場合" do
@@ -24,7 +27,7 @@ RSpec.describe "Protectors", type: :request do
       end
     end
 
-    context "ログイン状態の場合" do
+    context "protectorがログインしている場合" do
       before do
         sign_in protector
         get protector_path(protector.id)
@@ -48,6 +51,41 @@ RSpec.describe "Protectors", type: :request do
 
       it "@relationshipsが取得できていること" do
         expect(assigns(:relationships)).to eq [relationship]
+      end
+    end
+
+    context "userがログインしている場合" do
+      before do
+        sign_in user
+        get protector_path(protector.id)
+      end
+
+      it "リクエストが成功すること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "showテンプレートが表示されること" do
+        expect(response).to render_template :show
+      end
+
+      it "@protectorが取得できていること" do
+        expect(assigns(:protector)).to eq protector
+      end
+
+      it "@dogsが取得できていること" do
+        expect(assigns(:dogs)).to eq dogs
+      end
+
+      it "@relationshipsが取得できていること" do
+        expect(assigns(:relationships)).to eq [relationship]
+      end
+
+      it "@entryuserが取得できていること" do
+        expect(assigns(:entryuser)).to eq [entry]
+      end
+
+      it "@entryprotectorが取得できていること" do
+        expect(assigns(:entryprotector)).to eq [entry]
       end
     end
   end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   let!(:user)         { create(:user) }
-  let!(:relationship) { create(:relationship, user: user) }
+  let!(:protector)    { create(:protector) }
+  let!(:relationship) { create(:relationship, user: user, protector: protector) }
+  let!(:room)         { create(:room) }
+  let!(:entry)        { create(:entry, user: user, protector: protector, room: room) }
 
   describe "GET /show" do
     context "ログインしていない場合" do
@@ -23,7 +26,7 @@ RSpec.describe "Users", type: :request do
       end
     end
 
-    context "ログイン状態の場合" do
+    context "userがログインしている場合" do
       before do
         sign_in user
         get user_path(user.id)
@@ -43,6 +46,37 @@ RSpec.describe "Users", type: :request do
 
       it "@relationshipsが取得できていること" do
         expect(assigns(:relationships)).to eq [relationship]
+      end
+    end
+
+    context "protectorがログインしている場合" do
+      before do
+        sign_in protector
+        get user_path(user.id)
+      end
+
+      it "リクエストが成功すること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "showテンプレートが表示されること" do
+        expect(response).to render_template :show
+      end
+
+      it "@userが取得できていること" do
+        expect(assigns(:user)).to eq user
+      end
+
+      it "@relationshipsが取得できていること" do
+        expect(assigns(:relationships)).to eq [relationship]
+      end
+
+      it "@entryprotectorが取得できていること" do
+        expect(assigns(:entryprotector)).to eq [entry]
+      end
+
+      it "@entryuserが取得できていること" do
+        expect(assigns(:entryuser)).to eq [entry]
       end
     end
   end
