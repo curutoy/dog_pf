e4afe8705aa7bcfe47624dcad654204e949e8cbc
Mariko Katsuno <curureeno@gmail.com>
2021-01-08 18:00:25 +0900

    Add: notification_desc

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,12 +3,12 @@ class NotificationsController < ApplicationController
 
   def index
     if protector_signed_in?
-      @notifications = current_protector.passive_notifications.includes({ visitor_user: [image_attachment: :blob] }, :dog).page(params[:page]).per(20)
+      @notifications = current_protector.passive_notifications.includes({ visitor_user: [image_attachment: :blob] }, :dog).page(params[:page]).per(20).order('id DESC')
       @notifications.where(checked: false).each do |notification|
         notification.update_attributes(checked: true)
       end
     else
-      @notifications = current_user.passive_notifications.includes({ visitor_protector: [image_attachment: :blob] }, :dog).page(params[:page]).per(20)
+      @notifications = current_user.passive_notifications.includes({ visitor_protector: [image_attachment: :blob] }, :dog).page(params[:page]).per(20).order('id DESC')
       @notifications.where(checked: false).each do |notification|
         notification.update_attributes(checked: true)
       end
