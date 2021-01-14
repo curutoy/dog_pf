874682683848f720b7bf14d8088786d1733e86b7
Mariko Katsuno <curureeno@gmail.com>
2021-01-14 14:22:36 +0900

    Change: login_header/Spec: home_requests

--- spec/requests/homes_request_spec.rb
+++ spec/requests/homes_request_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
+  describe "GET /index" do
+    context "ログインしていない場合" do
+      before do
+        get homes_path
+      end
 
+      it "status codeは200となること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "indexテンプレートが表示されること" do
+        expect(response).to render_template :index
+      end
+    end
+
+    context "userでログイン状態の場合" do
+      before do
+        get homes_path
+      end
+
+      it "status codeは200となること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "indexテンプレートが表示されること" do
+        expect(response).to render_template :index
+      end
+    end
+
+    context "protectorでログイン状態の場合" do
+      before do
+        get homes_path
+      end
+
+      it "status codeは200となること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "indexテンプレートが表示されること" do
+        expect(response).to render_template :index
+      end
+    end
+  end
 end
